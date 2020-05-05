79214b07ff8e71c8dc77126eb71bd43a9bb35907
petercolella <peter.joseph.colella@gmail.com>
2020-05-05 10:55:30 -0400

    Updates

---
+++ precache-manifest.4ca35b40b59761689cd0b1d4ad58b1bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec8ab12b94bd90a5a256012e249ae38",
+    "url": "/pupster-deploy/index.html"
+  },
+  {
+    "revision": "162a6034efa96aa34e31",
+    "url": "/pupster-deploy/static/css/main.97f1e7cb.chunk.css"
+  },
+  {
+    "revision": "58dba90c9912031c114d",
+    "url": "/pupster-deploy/static/js/2.b392fa8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pupster-deploy/static/js/2.b392fa8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162a6034efa96aa34e31",
+    "url": "/pupster-deploy/static/js/main.5b977df6.chunk.js"
+  },
+  {
+    "revision": "542160f936187a397ab5",
+    "url": "/pupster-deploy/static/js/runtime-main.c41e9f4f.js"
+  }
+]);
